ac9504b91dee5a3275b2b4a81743a71a220994a7
sonhmle <sonhmle@kms-technology.com>
2022-01-08 23:05:48 +0700

    [semver:patch] fix env and param test

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,5 +1,5 @@
 Greet() {
-    echo Hello "${PARAM_TO}" and "${PARAM_AND}" for "$TO_NAME" of "$CIRCLE_BRANCH"
+    echo Hello "${PARAM_TO}" and "${PARAM_AND}" for "$TO_NAME" of "$BRANCH_IS"
 }
 
 # Will not run if sourced for bats-core tests.
