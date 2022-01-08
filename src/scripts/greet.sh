c1c4e44840f317894a4c49f419ed634d14fd3a6c
sonhmle <sonhmle@kms-technology.com>
2022-01-08 23:38:35 +0700

    [semver:patch] change env to string

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,10 +1,10 @@
 Greet() {
-    echo Hello "${PARAM_TO}" and "${PARAM_AND}" for "$TO_NAME" of "$BRANCH_IS"
+    echo Hello "${PARAM_TO}" and "${PARAM_AND}" for "$TO_NAME" of "$PARAM_BRANCH"
 }
 
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 # ORB_TEST_ENV="bats-core"
-if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-    Greet
-fi
+# if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+Greet
+# fi
