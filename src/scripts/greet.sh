b1659d8fcaf11a75dfd4628e8cc46cfd905adbb1
sonhmle <sonhmle@kms-technology.com>
2022-01-09 01:20:53 +0700

    [semver:patch] simple test again

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,5 +1,13 @@
 Greet() {
     echo "$PARAM_BRANCH"
+    echo "${PARAM_BRANCH:-}"
+    echo "${PARAM_BRANCH}"
+    echo "$PARAM_BRANCHES"
+    echo "${PARAM_BRANCHES:-}"
+    echo "${PARAM_BRANCHES}"
+    echo "$PARAM_BRANCH_NEXT"
+    echo "${PARAM_BRANCH_NEXT:-}"
+    echo "${PARAM_BRANCH_NEXT}"
 }
 
 Greet
