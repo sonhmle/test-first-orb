90503227696bc6bc920a67641a31d76805affc95
Son Hoang Minh Le <89216024+sonhmle@users.noreply.github.com>
2022-01-08 17:46:11 +0700

    [semver:minor] fix greet command (#4)

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -5,6 +5,6 @@ Greet() {
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 # ORB_TEST_ENV="bats-core"
-# if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-#     Greet
-# fi
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    Greet
+fi
