0a36398d76e2728dfbeb1faece5c8964becfc6fa
Son Hoang Minh Le <89216024+sonhmle@users.noreply.github.com>
2022-01-09 01:00:48 +0700

    [semver:patch] get direct env item (#16)

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,10 +1,5 @@
 Greet() {
-    echo "Hello ${PARAM_TO:-} and ${PARAM_AND:-} for ${TO_NAME} of ${PARAM_BRANCH:-}"
+    echo "Hello ${TO_NAME} and ${AND_NAME} of ${PARAM_BRANCH}"
 }
 
-# Will not run if sourced for bats-core tests.
-# View src/tests for more information.
-# ORB_TEST_ENV="bats-core"
-# if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
 Greet
-# fi
