cd8485ba395009e2f15408d4efdf8fb31fae8dee
Son Hoang Minh Le <89216024+sonhmle@users.noreply.github.com>
2022-01-09 09:25:58 +0700

    [semver:patch] fix param sh again (#22)

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,5 +1,6 @@
 Greet() {
-    echo "Hello ${<<parameters.to>>} and ${<<parameters.and>>} of ${<<parameters.branch>>}"
+    echo Hello "${<<parameters.to>>}" and "$<<parameters.and>>" of "$<<parameters.branch>>"
+    printf '\nbranch: %s \n' "$CIRCLE_BRANCH"
 }
 
 Greet
