d520e7620af479197c41bd4477b1858220b10351
Son Hoang Minh Le <89216024+sonhmle@users.noreply.github.com>
2022-01-09 15:04:08 +0700

    [semver:patch] fix type (#24)
    * [semver:patch] add to-and env param (#8)
    
    * [semver:patch] fix env and param test (#9)
    
    * [semver:patch] change env to string (#10)
    
    * [semver:patch] fix env param (#11)
    
    * [semver:patch] test env again like slack (#12)
    
    * [semver:patch] fix message (#13)
    
    * [semver:patch] fix env param again (#14)
    
    * [semver:patch] fix env param agains (#15)
    
    * [semver:patch] get direct env item (#16)
    
    * [semver:patch] simple test (#17)
    
    * [semver:patch] simple test again (#18)
    
    * [semver:patch] direct command (#19)
    
    * [semver:patch] param for sh (#20)
    
    * [semver:patch] fix param sh (#21)
    
    * [semver:patch] fix param sh again (#22)
    
    * [semver:minor] switch to slack
    
    * [semver:patch] fix type

--- src/scripts/README.md
+++ src/scripts/README.md
@@ -34,7 +34,7 @@ Greet() {
 
 # Will not run if sourced from another script. This is done so this script may be tested.
 # View src/tests for more information.
-if [[ "$_" == "$0" ]]; then
-    Greet
-fi
+# if [[ "$_" == "$0" ]]; then
+#     Greet
+# fi
 ```
---
+++ src/scripts/notify.sh
@@ -0,0 +1,16 @@
+# Greet() {
+#     echo "Hello ${<<parameters.to>>}" and "$<<parameters.and>>" of "$<<parameters.branch>>"
+#     printf '\nbranch: %s \n' "$CIRCLE_BRANCH"
+# }
+
+CheckEnvVars() {
+    if [ -z "${SLACK_PARAM_CHANNEL:-}" ]; then
+       echo "No channel was provided. Enter value for SLACK_DEFAULT_CHANNEL env var, or channel parameter"
+       exit 1
+    fi
+    echo "It ${CIRCLE_BRANCH}"
+    echo "Hello ${SLACK_PARAM_CHANNEL}"
+}
+
+# Greet
+CheckEnvVars
