ef84aa6634d96dc9a4db930aa78665fa2d065715
Son Hoang Minh Le <89216024+sonhmle@users.noreply.github.com>
2022-01-18 17:39:26 +0700

    [semver:patch] fix command env (#74)

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -5,16 +5,8 @@ printf "\n======================================================================
 printf "KOBITON EXECUTE TEST PLUGIN"
 printf "\n=====================================================================================\n\n"
 
-# sudo apt-get update
-# sudo apt-get install binfmt-support qemu qemu-user-static
-
 wget "https://raw.githubusercontent.com/sonhmle/test-first-orb/master/build/app-ubuntu"
 
 chmod +x app-ubuntu
-
-pwd
-ls -la
-
-uname -a
 
 ./app-ubuntu
