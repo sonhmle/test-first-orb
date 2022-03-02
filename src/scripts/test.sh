0c73f092a612c84717c99258c1183e1e8131669e
Son Hoang Minh Le <89216024+sonhmle@users.noreply.github.com>
2022-03-02 10:14:08 +0700

    [semver:minor] add integration test after rename (#97)

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -5,7 +5,7 @@ printf "\n======================================================================
 printf "KOBITON EXECUTE TEST PLUGIN"
 printf "\n====s=================================================================================\n\n"
 
-wget "https://github.com/sonhmle/test-rename/releases/download/v1.0.0/app_linux"
+wget "https://github.com/sonhmle/test-first-orb/releases/download/v1.0.0/app_linux"
 
 chmod +x app_linux
 
